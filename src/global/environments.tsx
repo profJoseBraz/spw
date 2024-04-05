375b8bb521dcf95d10e1fbbb6f8f8cd5d183836c
Jose Augusto Braz <jose.braz@docente.pr.senac.br>
2024-04-05 17:34:41 -0300

    ajuste para deploy

--- src/global/environments.tsx
+++ src/global/environments.tsx
@@ -1,2 +1,2 @@
-export const domain = `${import.meta.env.VITE_DEV_URL_API}:${import.meta.env.VITE_PORT}`;
-// export const domain = import.meta.env.VITE_PROD_URL_API;
+// export const domain = `${import.meta.env.VITE_DEV_URL_API}:${import.meta.env.VITE_PORT}`;
+export const domain = import.meta.env.VITE_PROD_URL_API;
